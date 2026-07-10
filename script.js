18afb6a2c09f8aaf291e2986d5cb1231677e6edf
kaar-codes <webdesignkaar@gmail.com>
2026-07-10 15:16:47 +0530

    Boxicons and Vite

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
 
+import { createElement, Battery2 } from "@boxicons/js";
+
 import { StatsCardComponent } from "./components/StatsComponent.js";
 import { RecentBookingsComponent } from "./components/RecentBookings.js";
 
 const root = document.getElementById("root");
+
+const alarmCheck = createElement(Battery2);
+root.append(alarmCheck);
 
 root.append(StatsCardComponent());
 root.append(RecentBookingsComponent());
